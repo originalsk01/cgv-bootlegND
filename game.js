e3f7bd8ef0fa2080a0f2ccd99560006044a5aa69
Sipho SK <sipssman@gmail.com>
2021-05-19 11:30:19 +0200

    Attempt at building a world

--- game.js
+++ game.js
@@ -1,23 +1,57 @@
-import * as THREE from './three.js-master/build/three.module.js';
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+console.log("Hello World")
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight,0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+
+//colours
+var black = "rgb(0,0,0)";
+var white = "rgb(255,255,255)";
+var red = "rgb(255,0,0)";
+var green = "rgb(10,200,10)";
+
+renderer.setClearColor(black);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+var axes = new THREE.AxesHelper(30);
+scene.add(axes);
+
+//plane
+var planeGeometry = new THREE.PlaneGeometry(500,500,1,1);
+var planeMaterial = new THREE.MeshBasicMaterial({color: green});
+var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = -0.5 * Math.PI;
+scene.add(plane);
+
+//cube
+var cubeGeometry = new THREE.BoxGeometry(6,6,6);
+var cubeMaterial = new THREE.MeshLambertMaterial({color: red});
+var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+cube.position.x = -10;
+cube.position.y = 3;
 scene.add(cube);
 
-camera.position.z = 3;
-var h = 0
-function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    h += 0.008
-    cube.material.color.setHSL(h, 1, 0.5)
-    renderer.render(scene, camera);
-}
-animate();
+//sphere
+var sphereGeometry = new THREE.SphereGeometry(4,20,20);
+var sphereMaterial = new THREE.MeshLambertMaterial({color: red});
+var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.y = 4
+sphere.position.x = 10;
+scene.add(sphere);
+
+//spotlight
+var spotlight = new THREE.SpotLight(0xffffff);
+spotlight.position.set(-40, 60, 40);
+scene.add(spotlight)
+
+
+
+camera.position.x = 10;
+camera.position.y = 10;
+camera.position.z = 50;
+camera.lookAt(scene.position);
+
+
+$("#gameCanvas").append(renderer.domElement);
+renderer.render(scene,camera);
