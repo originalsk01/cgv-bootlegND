353163be196b188e6a0793cda5e93b1f43fe99e5
pangsthemans <langiwemwale@gmail.com>
2021-05-19 22:39:32 +0200

    made the cube spin sheesshh

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
-import {GLTFLoader} from "./js/GLTFLoader.js"
+import { GLTFLoader } from "./js/GLTFLoader.js"
 import { RGBDEncoding } from "./js/three.module.js";
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const loader = new GLTFLoader();
 
 
@@ -26,30 +26,30 @@ var obj;
 loader.load(
     'thermos/scene.gltf',
 
-    function(gltf){
+    function (gltf) {
         obj = gltf.scene;
-        scene.add( gltf.scene );
+        scene.add(gltf.scene);
     }
 )
 
 //plane
-var planeGeometry = new THREE.PlaneGeometry(500,500,1,1);
-var planeMaterial = new THREE.MeshBasicMaterial({color: green});
+var planeGeometry = new THREE.PlaneGeometry(500, 500, 1, 1);
+var planeMaterial = new THREE.MeshBasicMaterial({ color: green });
 var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = -0.5 * Math.PI;
 scene.add(plane);
 
 //cube
-var cubeGeometry = new THREE.BoxGeometry(6,6,6);
-var cubeMaterial = new THREE.MeshLambertMaterial({color: red});
+var cubeGeometry = new THREE.BoxGeometry(6, 6, 6);
+var cubeMaterial = new THREE.MeshLambertMaterial({ color: red });
 var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 cube.position.x = -10;
 cube.position.y = 3;
 scene.add(cube);
 
 //sphere
-var sphereGeometry = new THREE.SphereGeometry(4,20,20);
-var sphereMaterial = new THREE.MeshLambertMaterial({color: red});
+var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+var sphereMaterial = new THREE.MeshLambertMaterial({ color: red });
 var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 sphere.position.y = 4
 sphere.position.x = 10;
@@ -62,39 +62,44 @@ scene.add(spotlight)
 
 //First person Controls section
 var cameraControls = new THREE.FirstPersonControls(camera);
+// cameraControls.lookSpeed = 0.05
+// cameraControls.movementSpeed = 5
 
 
-
-window.addEventListener('resize', function(){
-    renderer.setSize(window.innerWidth,window.innerHeight);
-    camera.aspect=window.innerWidth/window.innerHeight;
+window.addEventListener('resize', function () {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
 
 })
 
 
 //Initial camera position
-camera.position.x = 10;
-camera.position.y = 10;
+camera.position.x = 50;
+camera.position.y = 50;
 camera.position.z = 50;
 camera.lookAt(scene.position);
 
 var clock = new THREE.Clock();
 
 var step = 0;
-function renderScene(){
+var rad = 2;
+cube.position.z=-20
+cube.position.x=0
+cube.position.y=4
+function renderScene() {
     //Before request add changes in rotation and camera movements
     step += 0.005;
-    camera.position.x = 60*Math.cos(step);
-    camera.position.z = 60*Math.sin(step); 
+    
     camera.lookAt(scene.position);
-
+    cube.position.z+=Math.sin(step*Math.PI*rad)
+    cube.position.x+=Math.cos(step*Math.PI*rad)
     // var delta = clock.getDelta();//Gives time from when it was last called
     // cameraControls.update(delta);//Updates position
-    // renderer.clear();
+    renderer.clear();
 
 
     requestAnimationFrame(renderScene);//request render scene at every frame
-    renderer.render(scene,camera);
+    renderer.render(scene, camera);
 }
 
 $("#gameCanvas").append(renderer.domElement);
