805ce3e359a1154b0c990f6e1abff9a59efd9c9f
pangsthemans <langiwemwale@gmail.com>
2021-05-26 11:16:26 +0200

    cleaning and moving camera with snake

--- game.js
+++ game.js
@@ -2,23 +2,65 @@ import { GLTFLoader } from './js/GLTFLoader.js'
 import { FBXLoader } from './js/FBXLoader.js'
 import { OrbitControls } from './js/OrbitControls.js';
 
-import { RGBDEncoding } from './js/three.module.js';
-('use strict')
+// import { RGBDEncoding } from './js/three.module.js';
 var jumpheight=10
-var scene = new THREE.Scene()
-scene.background = new THREE.Color(0xa0a0a0)
-// var scene = new Physijs.Scene;
-// scene.setGravity(new THREE.Vector3(0, -10, 0));
+var scene,camera
+var snakeobj , mixer2
+var spotlight = new THREE.SpotLight(0xffffff)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.1)
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444)
+const dirLight = new THREE.DirectionalLight(0xffffff)
+//colours
+var black = 'rgb(0,0,0)'
+var white = 'rgb(255,255,255)'
+var red = 'rgb(255,0,0)'
+var green = 'rgb(10,200,10)'
+var blue = 'rgb(100,177,255)'
 
-var camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    1,
-    2000
-)
+
+
+init()
+function init(){
+    scene = new THREE.Scene()
+    scene.background = new THREE.Color(0xa0a0a0)
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        1,
+        2000
+    )
+    
+    //spotlight
+    spotlight.position.set(-40, 60, 40)
+    scene.add(spotlight)
+    //directionalLight light
+    directionalLight.position.set(0, 10, 10)
+    scene.add(directionalLight)
+
+    //HEMILIGHT??
+    hemiLight.position.set(0, 200, 0)
+    scene.add(hemiLight)
+
+    dirLight.position.set(0, 200, 100)
+    dirLight.castShadow = true
+    dirLight.shadow.camera.top = 180
+    dirLight.shadow.camera.bottom = -100
+    dirLight.shadow.camera.left = -120
+    dirLight.shadow.camera.right = 120
+    scene.add(dirLight)
+
+    var axes = new THREE.AxesHelper(30)
+    scene.add(axes)
+
+    
+}
+
+//
+
+
 
 //Animated model
-let mixer
+var mixer
 const loader = new FBXLoader()
 loader.load('character/Samba Dancing.fbx', (fbx) => {
     mixer = new THREE.AnimationMixer(fbx)
@@ -38,10 +80,9 @@ loader.load('character/Samba Dancing.fbx', (fbx) => {
     scene.add(fbx)
 })
 
-//var snakeobj = new THREE.Object3D;
-var snakeobj
+var snakeobj = new THREE.Object3D;
+
 var newLoader = new GLTFLoader()
-var mixer2
 newLoader.load('./resources/snake/scene.gltf', function (gltf) {
     snakeobj = gltf.scene;
     mixer2 = new THREE.AnimationMixer(gltf.scene.children[0]);
@@ -51,38 +92,18 @@ newLoader.load('./resources/snake/scene.gltf', function (gltf) {
 })
 scene.add(snakeobj);
 
-//var snakeobj = new THREE.Object3D;
-// var newLoader = new GLTFLoader()
-// newLoader.load('./resources/snake/scene.gltf', function (gltf) {
-//   snakeobj.add(gltf.scene.children[0]);
-// })
-// scene.add(snakeobj);
-
-//Space button pressed function
-
 
 var renderer = new THREE.WebGLRenderer()
 
-//colours
-var black = 'rgb(0,0,0)'
-var white = 'rgb(255,255,255)'
-var red = 'rgb(255,0,0)'
-var green = 'rgb(10,200,10)'
-var blue = 'rgb(110,197,233)'
 
 renderer.setClearColor(blue)
 renderer.setSize(window.innerWidth, window.innerHeight)
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.target.set(0, 100, 0)
-controls.update()
 
-var axes = new THREE.AxesHelper(30)
-scene.add(axes)
+
 
 //Load textures
 var groundTexture = new THREE.TextureLoader().load('./Rock_041_SD/Rock_041_ambientOcclusion.jpg');
-var altGroundTexture = new THREE.TextureLoader().load('rubble.jpeg');
 
 //plane
 var planeGeometry = new THREE.PlaneGeometry(500, 500, 1, 1)
@@ -93,44 +114,15 @@ var plane = new THREE.Mesh(planeGeometry, planeMaterial)
 plane.rotation.x = -0.5 * Math.PI
 scene.add(plane)
 
-//cube
-var cubeGeometry = new THREE.BoxGeometry(6, 6, 6)
-var cubeMaterial = new THREE.MeshLambertMaterial({ color: blue })
-var cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
-cube.position.x = -10
-cube.position.y = 3
-scene.add(cube)
 
 
-//spotlight
-var spotlight = new THREE.SpotLight(0xffffff)
-spotlight.position.set(-40, 60, 40)
-scene.add(spotlight)
-
-//
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.1)
-directionalLight.position.set(0, 10, 10)
-scene.add(directionalLight)
-
-//More lights
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444)
-hemiLight.position.set(0, 200, 0)
-scene.add(hemiLight)
-
-const dirLight = new THREE.DirectionalLight(0xffffff)
-dirLight.position.set(0, 200, 100)
-dirLight.castShadow = true
-dirLight.shadow.camera.top = 180
-dirLight.shadow.camera.bottom = -100
-dirLight.shadow.camera.left = -120
-dirLight.shadow.camera.right = 120
-scene.add(dirLight)
 
 
 window.addEventListener('resize', function () {
     renderer.setSize(window.innerWidth, window.innerHeight)
     camera.aspect = window.innerWidth / window.innerHeight
 })
+
 
 document.body.addEventListener('keydown', keyPressed);
 
@@ -149,7 +141,7 @@ function keyPressed(e) {
             snakeobj.position.x += -1;
             break;
         case " ": // check if the space button pressed
-            snakeobj.position.y+=10
+            snakeobj.position.y+=jumpheight
             
     }
     e.preventDefault();
@@ -157,38 +149,37 @@ function keyPressed(e) {
 }
 
 //Initial camera position
-camera.position.set(50, 50, 50)
 
 var clock = new THREE.Clock()
 
 var step = 0
-var rad = 2
-cube.position.z = -20
-cube.position.x = 0
-cube.position.y = 4
+
+function updateSnake(delta){
+    if (snakeobj) {//checks if snake object defined before doing loop for jump
+        if(snakeobj.position.y>=jumpheight){
+            snakeobj.position.y=jumpheight // set max height for jump
+        }
+        if(snakeobj.position.y>0 ){//check if the snake is above groun
+    
+            snakeobj.position.y -= delta*20// make snake fall at rate of loop animation
+        }
+    }
+
+}
+
+
 function renderScene() {
     //Model
+    camera.position.set(snakeobj.position.x,snakeobj.position.y+10,snakeobj.position.z-10);//camera will always be set to the snakes current position by some offset
     const delta = clock.getDelta()
 
     if (mixer) mixer.update(delta)
     if (mixer2) mixer2.update(delta)
-    if (snakeobj) {//checks if snake object defined before doing loop for jump
-        if(snakeobj.position.y>=10){
-            snakeobj.position.y=10 // set max height for jump
-        }
-        if(snakeobj.position.y>0 ){//check if the snake is above groun
-
-            snakeobj.position.y -= delta*20// make snake fall at rate of loop animation
-        }
-        
-        
-    }
+    updateSnake(delta);
     //Before request add changes in rotation and camera movements
     step += 0.005
 
-    camera.lookAt(scene.position)
-    //   cube.position.z += Math.sin(step * Math.PI * rad)
-    //   cube.position.x += Math.cos(step * Math.PI * rad)
+    camera.lookAt(snakeobj.position.x+5,snakeobj.position.y+5,snakeobj.position.z+10)
     // var delta = clock.getDelta();//Gives time from when it was last called
     // cameraControls.update(delta);//Updates position
     renderer.clear()
