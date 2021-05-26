561d87f28ccd094280acf5d7dada7f6227baaf8f
pangsthemans <langiwemwale@gmail.com>
2021-05-26 10:15:21 +0200

    added jumping to snake

--- game.js
+++ game.js
@@ -2,40 +2,40 @@ import { GLTFLoader } from './js/GLTFLoader.js'
 import { FBXLoader } from './js/FBXLoader.js'
 import { OrbitControls } from './js/OrbitControls.js';
 
-import { RGBDEncoding } from './js/three.module.js'
-;('use strict')
-
+import { RGBDEncoding } from './js/three.module.js';
+('use strict')
+var jumpheight=10
 var scene = new THREE.Scene()
 scene.background = new THREE.Color(0xa0a0a0)
 // var scene = new Physijs.Scene;
 // scene.setGravity(new THREE.Vector3(0, -10, 0));
 
 var camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  1,
-  2000
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    2000
 )
 
 //Animated model
 let mixer
 const loader = new FBXLoader()
 loader.load('character/Samba Dancing.fbx', (fbx) => {
-  mixer = new THREE.AnimationMixer(fbx)
-  fbx.scale.set(.15,.15,.15);
-  fbx.position.set(25,0,0);
+    mixer = new THREE.AnimationMixer(fbx)
+    fbx.scale.set(.15, .15, .15);
+    fbx.position.set(25, 0, 0);
 
-  const action = mixer.clipAction(fbx.animations[0])
-  action.play()
+    const action = mixer.clipAction(fbx.animations[0])
+    action.play()
 
-  fbx.traverse(function (child) {
-    if (child.isMesh) {
-      child.castShadow = true
-      child.receiveShadow = true
-    }
-  })
+    fbx.traverse(function (child) {
+        if (child.isMesh) {
+            child.castShadow = true
+            child.receiveShadow = true
+        }
+    })
 
-  scene.add(fbx)
+    scene.add(fbx)
 })
 
 //var snakeobj = new THREE.Object3D;
@@ -43,10 +43,10 @@ var snakeobj
 var newLoader = new GLTFLoader()
 var mixer2
 newLoader.load('./resources/snake/scene.gltf', function (gltf) {
-  snakeobj=gltf.scene;
-  mixer2= new THREE.AnimationMixer(gltf.scene.children[0]);
-  gltf.animations.forEach((clip) => {mixer2.clipAction(clip).play(); });
-  scene.add(gltf.scene)
+    snakeobj = gltf.scene;
+    mixer2 = new THREE.AnimationMixer(gltf.scene.children[0]);
+    gltf.animations.forEach((clip) => { mixer2.clipAction(clip).play(); });
+    scene.add(gltf.scene)
 
 })
 scene.add(snakeobj);
@@ -57,6 +57,8 @@ scene.add(snakeobj);
 //   snakeobj.add(gltf.scene.children[0]);
 // })
 // scene.add(snakeobj);
+
+//Space button pressed function
 
 
 var renderer = new THREE.WebGLRenderer()
@@ -79,14 +81,14 @@ var axes = new THREE.AxesHelper(30)
 scene.add(axes)
 
 //Load textures
-var groundTexture = new THREE.TextureLoader().load('./Rock_041_SD/Rock_041_ambientOcclusion.jpg' );
-var altGroundTexture = new THREE.TextureLoader().load('rubble.jpeg' );
+var groundTexture = new THREE.TextureLoader().load('./Rock_041_SD/Rock_041_ambientOcclusion.jpg');
+var altGroundTexture = new THREE.TextureLoader().load('rubble.jpeg');
 
 //plane
 var planeGeometry = new THREE.PlaneGeometry(500, 500, 1, 1)
 //using standard material so its got the same properties as unity rendering
 //meaning its affected by lights , basic means its unaffected by lights
-var planeMaterial = new THREE.MeshBasicMaterial({ map:groundTexture })
+var planeMaterial = new THREE.MeshBasicMaterial({ map: groundTexture })
 var plane = new THREE.Mesh(planeGeometry, planeMaterial)
 plane.rotation.x = -0.5 * Math.PI
 scene.add(plane)
@@ -126,26 +128,29 @@ scene.add(dirLight)
 
 
 window.addEventListener('resize', function () {
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  camera.aspect = window.innerWidth / window.innerHeight
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    camera.aspect = window.innerWidth / window.innerHeight
 })
 
 document.body.addEventListener('keydown', keyPressed);
 
-function keyPressed(e){
-    switch(e.key) {
-      case 'ArrowUp':
-          snakeobj.position.z+=1;
-          break;
-      case 'ArrowDown':
-        snakeobj.position.z+=-1;
-        break;
-      case 'ArrowLeft':
-          snakeobj.position.x+=1;
-          break;
-      case 'ArrowRight':
-          snakeobj.position.x+=-1;
-          break;
+function keyPressed(e) {
+    switch (e.key) {
+        case 'ArrowUp':
+            snakeobj.position.z += 1;
+            break;
+        case 'ArrowDown':
+            snakeobj.position.z += -1;
+            break;
+        case 'ArrowLeft':
+            snakeobj.position.x += 1;
+            break;
+        case 'ArrowRight':
+            snakeobj.position.x += -1;
+            break;
+        case " ": // check if the space button pressed
+            snakeobj.position.y+=10
+            
     }
     e.preventDefault();
 
@@ -162,24 +167,34 @@ cube.position.z = -20
 cube.position.x = 0
 cube.position.y = 4
 function renderScene() {
-  //Model
-  const delta = clock.getDelta()
+    //Model
+    const delta = clock.getDelta()
 
-  if (mixer) mixer.update(delta)
-  if (mixer2) mixer2.update(delta)
+    if (mixer) mixer.update(delta)
+    if (mixer2) mixer2.update(delta)
+    if (snakeobj) {//checks if snake object defined before doing loop for jump
+        if(snakeobj.position.y>=10){
+            snakeobj.position.y=10 // set max height for jump
+        }
+        if(snakeobj.position.y>0 ){//check if the snake is above groun
 
-  //Before request add changes in rotation and camera movements
-  step += 0.005
+            snakeobj.position.y -= delta*20// make snake fall at rate of loop animation
+        }
+        
+        
+    }
+    //Before request add changes in rotation and camera movements
+    step += 0.005
 
-  camera.lookAt(scene.position)
-//   cube.position.z += Math.sin(step * Math.PI * rad)
-//   cube.position.x += Math.cos(step * Math.PI * rad)
-  // var delta = clock.getDelta();//Gives time from when it was last called
-  // cameraControls.update(delta);//Updates position
-  renderer.clear()
+    camera.lookAt(scene.position)
+    //   cube.position.z += Math.sin(step * Math.PI * rad)
+    //   cube.position.x += Math.cos(step * Math.PI * rad)
+    // var delta = clock.getDelta();//Gives time from when it was last called
+    // cameraControls.update(delta);//Updates position
+    renderer.clear()
 
-  requestAnimationFrame(renderScene) //request render scene at every frame
-  renderer.render(scene, camera)
+    requestAnimationFrame(renderScene) //request render scene at every frame
+    renderer.render(scene, camera)
 }
 
 $('#gameCanvas').append(renderer.domElement)
