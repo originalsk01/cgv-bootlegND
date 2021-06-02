37d74ce7b9daed62eaa51d2145c899dd75917757
MADeCarvalho <49862495+MADeCarvalho@users.noreply.github.com>
2021-06-02 17:20:20 +0200

    Bad third person ship
    Bad third person ship

--- game.js
+++ game.js
@@ -1,85 +1,93 @@
-import { GLTFLoader } from './js/libs/GLTFLoader.js'
-import { FBXLoader } from './js/libs/FBXLoader.js'
-import { OrbitControls } from './js/libs/OrbitControls.js'
-import * as THREE from './js/libs/three.module.js'
-import { PointerLockControls } from './js/libs/PointerLockControls.js'
+import { GLTFLoader } from "./js/libs/GLTFLoader.js";
+import { FBXLoader } from "./js/libs/FBXLoader.js";
+import { OrbitControls } from "./js/libs/OrbitControls.js";
+import * as THREE from "./js/libs/three.module.js";
+import { PointerLockControls } from "./js/libs/PointerLockControls.js";
 
 //import { RGBDEncoding } from './js/three.module.js';
 
-var jumpheight = 10
-var scene, camera
-var snakeobj, mixer2
-var spotlight = new THREE.SpotLight(0xffffff)
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.1)
-const hemiLight = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75)
-const dirLight = new THREE.DirectionalLight(0xffffff)
+var jumpheight = 10;
+var scene, camera;
+var snakeobj, mixer2;
+var spotlight = new THREE.SpotLight(0xffffff);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.1);
+const hemiLight = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75);
+const dirLight = new THREE.DirectionalLight(0xffffff);
 //colours
-var black = 'rgb(0,0,0)'
-var white = 'rgb(255,255,255)'
-var red = 'rgb(255,0,0)'
-var green = 'rgb(10,200,10)'
-var blue = 'rgb(100,177,255)'
+var black = "rgb(0,0,0)";
+var white = "rgb(255,255,255)";
+var red = "rgb(255,0,0)";
+var green = "rgb(10,200,10)";
+var blue = "rgb(100,177,255)";
 
-var mixer
-const loader = new FBXLoader()
-var snakeobj = new THREE.Object3D()
-var newLoader = new GLTFLoader()
-var clock = new THREE.Clock()
-var step = 0
+//Models and loaders
+const loader   = new FBXLoader();
+var snakeobj   = new THREE.Object3D();
+var newLoader  = new GLTFLoader();
+var shipModel  = new THREE.Object3D();
+var shipLoader = new GLTFLoader();
+var tokenModel  = new THREE.Object3D();
+var tokenLoader = new GLTFLoader();
 
-let controls
+var mixer;
+var clock = new THREE.Clock();
+var step = 0;
 
-let raycaster
+let controls;
+
+let raycaster;
 
 //Movement
-let moveForward = false
-let moveBackward = false
-let moveLeft = false
-let moveRight = false
-let canJump = false
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = false;
 
-let prevTime = performance.now()
-const velocity = new THREE.Vector3()
-const direction = new THREE.Vector3()
-const vertex = new THREE.Vector3()
-const color = new THREE.Color()
+let prevTime = performance.now();
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+const vertex = new THREE.Vector3();
+const color = new THREE.Color();
 
-init()
+init();
 function init() {
-  scene = new THREE.Scene()
-  scene.background = new THREE.Color(0xa0a0a0)
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xa0a0a0);
   camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     1,
     2000
-  )
+  );
+  // camera.position.set( 0, 50, - 10 );
+  // camera.lookAt(shipModel.position);
 
   //spotlight
   // spotlight.position.set(-40, 60, 40)
   // scene.add(spotlight)
   //directionalLight light
-  directionalLight.position.set(0, 10, 10)
-  scene.add(directionalLight)
+  directionalLight.position.set(0, 10, 10);
+  scene.add(directionalLight);
 
   //Ambient Light
-  const light = new THREE.AmbientLight(0x808080) // soft white light
-  scene.add(light)
+  const light = new THREE.AmbientLight(0x808080); // soft white light
+  scene.add(light);
 
   //HEMILIGHT??
-  hemiLight.position.set(0.5, 1, 0.75)
-  scene.add(hemiLight)
+  hemiLight.position.set(0.5, 1, 0.75);
+  scene.add(hemiLight);
 
-  dirLight.position.set(0, 200, 100)
-  dirLight.castShadow = true
-  dirLight.shadow.camera.top = 180
-  dirLight.shadow.camera.bottom = -100
-  dirLight.shadow.camera.left = -120
-  dirLight.shadow.camera.right = 120
-  scene.add(dirLight)
+  dirLight.position.set(0, 200, 100);
+  dirLight.castShadow = true;
+  dirLight.shadow.camera.top = 180;
+  dirLight.shadow.camera.bottom = -100;
+  dirLight.shadow.camera.left = -120;
+  dirLight.shadow.camera.right = 120;
+  scene.add(dirLight);
 
-  var axes = new THREE.AxesHelper(30)
-  scene.add(axes)
+  var axes = new THREE.AxesHelper(30);
+  scene.add(axes);
 
   //Load textures
   // var groundTexture = new THREE.TextureLoader().load('./textures/Rock_041_SD/Rock_041_ambientOcclusion.jpg');
@@ -91,247 +99,305 @@ function init() {
   // var plane = new THREE.Mesh(planeGeometry, planeMaterial)
   // plane.rotation.x = -0.5 * Math.PI
   // scene.add(plane)
-  var meshPlane = new THREE.Mesh(
-    new THREE.PlaneBufferGeometry(10000, 10000),
-    new THREE.MeshPhongMaterial({ color: 0x111111, depthWrite: false })
-  )
-  meshPlane.rotation.x = -Math.PI / 2
-  meshPlane.receiveShadow = true
-  scene.add(meshPlane)
-  //Grid for floor
-  var grid = new THREE.GridHelper(5000, 40, 0xff0000, 0x0000ff)
-  grid.material.opacity = 0.7
-  grid.material.transparent = true
-  scene.add(grid)
+
+  //Create Floor
+  // var meshPlane = new THREE.Mesh(
+  //   new THREE.PlaneBufferGeometry(10000, 10000),
+  //   new THREE.MeshPhongMaterial({ color: 0x111111, depthWrite: false })
+  // );
+  // meshPlane.rotation.x = -Math.PI / 2;
+  // meshPlane.receiveShadow = true;
+  // scene.add(meshPlane);
+  // //Grid for floor
+  // var grid = new THREE.GridHelper(5000, 40, 0xff0000, 0x0000ff);
+  // grid.material.opacity = 0.7;
+  // grid.material.transparent = true;
+  // scene.add(grid);
 
   //Skybox
-  const loader = new THREE.CubeTextureLoader()
+  const loader = new THREE.CubeTextureLoader();
   const texture = loader.load([
-    'textures/skybox/indigo_ft.jpg',
-    'textures/skybox/indigo_bk.jpg',
-    'textures/skybox/indigo_up.jpg',
-    'textures/skybox/indigo_dn.jpg',
-    'textures/skybox/indigo_rt.jpg',
-    'textures/skybox/indigo_lf.jpg',
-  ])
-  scene.background = texture
+    "textures/skybox/indigo_ft.jpg",
+    "textures/skybox/indigo_bk.jpg",
+    "textures/skybox/indigo_up.jpg",
+    "textures/skybox/indigo_dn.jpg",
+    "textures/skybox/indigo_rt.jpg",
+    "textures/skybox/indigo_lf.jpg",
+  ]);
+  scene.background = texture;
 
   //Create first person controls
-  controls = new PointerLockControls(camera, document.body)
+  controls = new PointerLockControls(shipModel, document.body);
 
   //blocker and instructions is used to pause and start game
-  const blocker = document.getElementById('blocker')
-  const instructions = document.getElementById('instructions')
+  const blocker = document.getElementById("blocker");
+  const instructions = document.getElementById("instructions");
 
-  instructions.addEventListener('click', function () {
-    controls.lock()
-  })
+  instructions.addEventListener("click", function() {
+    controls.lock();
+  });
 
-  controls.addEventListener('lock', function () {
-    instructions.style.display = 'none'
-    blocker.style.display = 'none'
-  })
+  controls.addEventListener("lock", function() {
+    instructions.style.display = "none";
+    blocker.style.display = "none";
+  });
 
-  controls.addEventListener('unlock', function () {
-    blocker.style.display = 'block'
-    instructions.style.display = ''
-  })
+  controls.addEventListener("unlock", function() {
+    blocker.style.display = "block";
+    instructions.style.display = "";
+  });
 
-  scene.add(controls.getObject())
+  scene.add(controls.getObject());
 
-  const onKeyDown = function (event) {
+  const onKeyDown = function(event) {
     switch (event.code) {
-      case 'ArrowUp':
-      case 'KeyW':
-        moveForward = true
-        break
+      case "ArrowUp":
+      case "KeyW":
+        moveForward = true;
+        break;
 
-      case 'ArrowLeft':
-      case 'KeyA':
-        moveLeft = true
-        break
+      case "ArrowLeft":
+      case "KeyA":
+        moveLeft = true;
+        break;
 
-      case 'ArrowDown':
-      case 'KeyS':
-        moveBackward = true
-        break
+      case "ArrowDown":
+      case "KeyS":
+        moveBackward = true;
+        break;
 
-      case 'ArrowRight':
-      case 'KeyD':
-        moveRight = true
-        break
+      case "ArrowRight":
+      case "KeyD":
+        moveRight = true;
+        break;
 
-      case 'Space':
-        if (canJump === true) velocity.y += 350
-        canJump = false
-        break
+      case "Space":
+        if (canJump === true) velocity.y += 350;
+        canJump = false;
+        break;
     }
-  }
+  };
 
-  const onKeyUp = function (event) {
+  const onKeyUp = function(event) {
     switch (event.code) {
-      case 'ArrowUp':
-      case 'KeyW':
-        moveForward = false
-        break
+      case "ArrowUp":
+      case "KeyW":
+        moveForward = false;
+        break;
 
-      case 'ArrowLeft':
-      case 'KeyA':
-        moveLeft = false
-        break
+      case "ArrowLeft":
+      case "KeyA":
+        moveLeft = false;
+        break;
 
-      case 'ArrowDown':
-      case 'KeyS':
-        moveBackward = false
-        break
+      case "ArrowDown":
+      case "KeyS":
+        moveBackward = false;
+        break;
 
-      case 'ArrowRight':
-      case 'KeyD':
-        moveRight = false
-        break
+      case "ArrowRight":
+      case "KeyD":
+        moveRight = false;
+        break;
     }
-  }
+  };
 
-  document.addEventListener('keydown', onKeyDown)
-  document.addEventListener('keyup', onKeyUp)
+  document.addEventListener("keydown", onKeyDown);
+  document.addEventListener("keyup", onKeyUp);
 
   raycaster = new THREE.Raycaster(
     new THREE.Vector3(),
     new THREE.Vector3(0, -1, 0),
     0,
     10
-  )
+  );
+
+  loadModels();
 }
 
-//Animated model
+//Function to load all models
+function loadModels() {
+  //Samba Dancer
+  loader.load("character/Samba Dancing.fbx", (fbx) => {
+    mixer = new THREE.AnimationMixer(fbx);
+    fbx.scale.set(0.15, 0.15, 0.15);
+    fbx.position.set(25, 0, 0);
 
-loader.load('character/Samba Dancing.fbx', (fbx) => {
-  mixer = new THREE.AnimationMixer(fbx)
-  fbx.scale.set(0.15, 0.15, 0.15)
-  fbx.position.set(25, 0, 0)
+    const action = mixer.clipAction(fbx.animations[0]);
+    action.play();
 
-  const action = mixer.clipAction(fbx.animations[0])
-  action.play()
+    fbx.traverse(function(child) {
+      if (child.isMesh) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    });
 
-  fbx.traverse(function (child) {
-    if (child.isMesh) {
-      child.castShadow = true
-      child.receiveShadow = true
-    }
-  })
+    scene.add(fbx);
+  });
 
-  scene.add(fbx)
-})
+  //Snake
+  newLoader.load("./resources/snake/scene.gltf", function(gltf) {
+    mixer2 = new THREE.AnimationMixer(gltf.scene.children[0]);
+    gltf.animations.forEach((clip) => {
+      mixer2.clipAction(clip).play();
+    });
+    // mixer2.clipAction(gltf.animations[0]).play()
+    snakeobj.add(gltf.scene);
+  });
+  scene.add(snakeobj);
 
-newLoader.load('./resources/snake/scene.gltf', function (gltf) {
-  mixer2 = new THREE.AnimationMixer(gltf.scene.children[0])
-  gltf.animations.forEach((clip) => {
-    mixer2.clipAction(clip).play()
-  })
-  // mixer2.clipAction(gltf.animations[0]).play()
-  snakeobj.add(gltf.scene)
-})
-scene.add(snakeobj)
+  //Starship
+  shipLoader.load("character/LPSP_SmallStarfigher.gltf", function(gltfModel) {
+    gltfModel.scene.scale.multiplyScalar(1.9);
+    gltfModel.scene.traverse(function(child) {
+      //console.log(child);
+    });
+    shipModel.add(gltfModel.scene);
+    
+  });
 
-var renderer = new THREE.WebGLRenderer({ antialias: true })
+  
+  scene.add(shipModel);
 
-renderer.setClearColor(blue)
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(window.innerWidth, window.innerHeight)
+  //Token that the playr collects
+  tokenLoader.load("character/token.gltf", function(gltfModel) {
+    gltfModel.scene.scale.multiplyScalar(0.1);
+    gltfModel.scene.traverse(function(child) {
+      //console.log(child);
+    });
+    tokenModel.add(gltfModel.scene);
+    
+  });
+
+  scene.add(tokenModel);
+}
+
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+
+renderer.setClearColor(blue);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 //when window resizes
-window.addEventListener('resize', function () {
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  camera.aspect = window.innerWidth / window.innerHeight
-})
+window.addEventListener("resize", function() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+});
 
 //Add boxes to the world
-const boxGeometry = new THREE.BoxGeometry(50, 10, 30).toNonIndexed()
+const boxGeometry = new THREE.BoxGeometry(50, 10, 30).toNonIndexed();
 
 // position = boxGeometry.attributes.position;
-const colorsBox = []
+const colorsBox = [];
 
 for (let i = 0, l = 36; i < l; i++) {
-  color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75)
-  colorsBox.push(color.r, color.g, color.b)
+  color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
+  colorsBox.push(color.r, color.g, color.b);
 }
 
 boxGeometry.setAttribute(
-  'color',
+  "color",
   new THREE.Float32BufferAttribute(colorsBox, 3)
-)
+);
 
 for (let i = 0; i < 2000; i++) {
   const boxMaterial = new THREE.MeshPhongMaterial({
     specular: 0xffffff,
     flatShading: true,
     vertexColors: true,
-  })
+  });
   boxMaterial.color.setHSL(
     Math.random() * 0.2 + 0.5,
     0.75,
     Math.random() * 0.25 + 0.75
-  )
+  );
 
-  const box = new THREE.Mesh(boxGeometry, boxMaterial)
-  box.position.x = Math.floor(Math.random() * 20 - 10) * 135 + Math.random() * 100 
-  box.position.y =  10
-  box.position.z = Math.floor(Math.random() * 20 - 10) * 100 +  Math.random() * 100
+  const box = new THREE.Mesh(boxGeometry, boxMaterial);
+  box.position.x =
+    Math.floor(Math.random() * 20 - 10) * 135 + Math.random() * 100;
+  box.position.y = 10;
+  box.position.z =
+    Math.floor(Math.random() * 20 - 10) * 100 + Math.random() * 100;
 
-  scene.add(box)
+  scene.add(box);
   // objects.push(box)
 }
 
 function renderScene() {
-  renderer.clear()
+  renderer.clear();
 
-  const delta = clock.getDelta()
+  const delta = clock.getDelta();
   //Animate snake and person
-  if (mixer) mixer.update(delta)
-  if (mixer2) mixer2.update(delta)
+  if (mixer) mixer.update(delta);
+  if (mixer2) mixer2.update(delta);
 
-  //Set snake position and direction
-  snakeobj.position.set(camera.position.x, camera.position.y-5, camera.position.z);
-  let cameraDirectionNormalised = camera.getWorldDirection();
-  let cameraDirection = new THREE.Vector3(camera.position.x + cameraDirectionNormalised.x, snakeobj.position.y , camera.position.z + cameraDirectionNormalised.z);
-  snakeobj.lookAt(cameraDirection);
+  //let cameraDirectionNormalised = new THREE.Vector3();
+  // shipModel.position.set(camera.position.x, camera.position.y-10, camera.position.z);
+  //shipModel.translateZ(camera.position.x, camera.position.y-10, camera.position.z+10);
+  //camera.getWorldDirection(cameraDirectionNormalised);
+  //let cameraDirection = new THREE.Vector3(camera.position.x + cameraDirectionNormalised.x, shipModel.position.y , camera.position.z + cameraDirectionNormalised.z);
+  //shipModel.lookAt(cameraDirection);
 
-  requestAnimationFrame(renderScene) //request render scene at every frame
-  const time = performance.now()
+  //var goal = new THREE.Object3D;
+  //var temp = new THREE.Vector3();
+  //goal.position.set( 0, 50, - 10 );
+  //temp.setFromMatrixPosition(goal.matrixWorld);
+  camera.position.set( shipModel.position.x-5, shipModel.position.y+10, shipModel.position.x - 10 );
+  camera.position.lerp(shipModel.position, 0.2);
+  camera.lookAt(shipModel.position);
+
+  requestAnimationFrame(renderScene); //request render scene at every frame
+  const time = performance.now();
 
   if (controls.isLocked === true) {
     // const delta = (time - prevTime) / 1000;
+    
+    //Set movement velocity in each direction
+    velocity.x -= velocity.x * 10.0 * delta;
+    velocity.z -= velocity.z * 10.0 * delta;
+    velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
 
-    velocity.x -= velocity.x * 10.0 * delta
-    velocity.z -= velocity.z * 10.0 * delta
+    //Determine direction since Number(direction) evaluates to true or false since 
+    //moveForward, moveBackwards, moveLeft and moveRight are all boolean values
+    //and so if moveForward is True(1) and moveBackward is false(0) then 1-0=1 and so we
+    //move in the positive z direction
+    direction.z = Number(moveForward) - Number(moveBackward);
+    direction.x = Number(moveRight) - Number(moveLeft);
+    direction.normalize(); // this ensures consistent movements in all directions
 
-    velocity.y -= 9.8 * 100.0 * delta // 100.0 = mass
+    //Move in direction
+    if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta;
 
-    direction.z = Number(moveForward) - Number(moveBackward)
-    direction.x = Number(moveRight) - Number(moveLeft)
-    direction.normalize() // this ensures consistent movements in all directions
+    if (moveLeft) {
+      velocity.x -= direction.x * 400.0 * delta;
+      shipModel.rotateY(0.2);
+    }
 
-    if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta
-    if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta
+    if (moveRight) {
+      velocity.x -= direction.x * 400.0 * delta;
+      shipModel.rotateY(-0.2);
+    }
 
-    controls.moveRight(-velocity.x * delta)
-    controls.moveForward(-velocity.z * delta)
+    controls.moveRight(-velocity.x * delta);
+    controls.moveForward(-velocity.z * delta);
 
-    controls.getObject().position.y += velocity.y * delta // new behavior
+    controls.getObject().position.y += velocity.y * delta; // new behavior
 
+    //If player height is below 10 then set vertical velocity to 0,
+    //without this the player falls through the map
     if (controls.getObject().position.y < 10) {
-      velocity.y = 0
-      controls.getObject().position.y = 10
-
-      canJump = true
+      velocity.y = 0;
+      controls.getObject().position.y = 10;
+      canJump = true;
     }
   }
 
-  prevTime = time
-  renderer.render(scene, camera)
+  prevTime = time;
+  renderer.render(scene, camera);
 }
 
-$('#gameCanvas').append(renderer.domElement)
+$("#gameCanvas").append(renderer.domElement);
 // var controls = new THREE.OrbitControls(camera, renderer.domElement)
 // controls.update()
-renderScene()
+renderScene();
