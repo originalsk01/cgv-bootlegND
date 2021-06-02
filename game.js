b8d8fb2e888a6abb10bbebddd8f2c8275acd3b88
blipOnGit <talionnaidoo@gmail.com>
2021-06-02 19:20:57 +0200

    Revert

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 import { BufferGeometryUtils } from 'three/examples/jsm/utils/BufferGeometryUtils.js'
-import Stats from "three/examples/jsm/libs/stats.module.js"
+import Stats from  "three/examples/jsm/libs/stats.module.js"
 import * as CANNON from 'cannon-es'
 
 import { threeToCannon, ShapeType } from 'three-to-cannon';
@@ -24,16 +24,12 @@ const blue = 'rgb(110,197,233)'
 // cannon-es global vars
 let world
 let sphereBody, shipBody
-const timeStep = 1 / 60
+const timeStep = 1 / 60 
 let lastCallTime
 
 
 // gameplay vars
-const jumpheight = 10
-
-
-//textures
-var loader,skyBoxtexture
+const jumpheight=10
 
 
 class Game {
@@ -322,66 +318,66 @@ async function loadModel(path){
 
 
 function animate() {
+	
+	// cannon-es world stepping
+	updatePhysics()
+	
 
-  // cannon-es world stepping
-  updatePhysics()
+	// three.js model positions updates using cannon-es simulation
+	sphereMesh.position.copy(sphereBody.position)
+	sphereMesh.quaternion.copy(sphereBody.quaternion)
 
+	//plat0.position.copy(plat0Body.position)
+	//plat0.quaternion.copy(plat0Body.quaternion)
+	
+	// models animations
+	const delta = clock.getDelta()
+	if (dancerMixer) dancerMixer.update(delta)
+	if (snakeMixer) snakeMixer.update(delta)
+  
+	// render three.js
 
-  // three.js model positions updates using cannon-es simulation
-  sphereMesh.position.copy(sphereBody.position)
-  sphereMesh.quaternion.copy(sphereBody.quaternion)
-
-  //plat0.position.copy(plat0Body.position)
-  //plat0.quaternion.copy(plat0Body.quaternion)
-
-  // models animations
-  const delta = clock.getDelta()
-  if (dancerMixer) dancerMixer.update(delta)
-  if (snakeMixer) snakeMixer.update(delta)
-
-  // render three.js
-
-  renderer.clear()
-  requestAnimationFrame(animate) //request render scene at every frame
-  renderer.render(scene, camera)
-  stats.update()
+	renderer.clear()
+	requestAnimationFrame(animate) //request render scene at every frame
+	renderer.render(scene, camera)
+	stats.update()
 }
 
 
 function onWindowResize() {
 
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
 
 }
 
 
-function updatePhysics() {
-
-  const time = performance.now() / 1000
-  if (!lastCallTime) {
-    world.step(timeStep)
-  } else {
-    const dt = time - lastCallTime
-    world.step(timeStep, dt)
-  }
-  lastCallTime = time
+function updatePhysics(){
+	
+	const time = performance.now() / 1000
+	if (!lastCallTime) {
+		world.step(timeStep)
+	} else {
+		const dt = time - lastCallTime
+		world.step(timeStep, dt)
+	}
+	lastCallTime = time	
 }
 
 
 // Randomizers that can be used for building Bufffer geometries
 
 // random integer within range
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+function getRandomInt(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
 // random float within range
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+function getRandomArbitrary(min, max){
+    return Math.random() * (max - min) + min;
 }
 
 
