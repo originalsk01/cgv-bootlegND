50cbd3927c82c407b14eb8ae0a0f747c1bd27ac3
pangsthemans <langiwemwale@gmail.com>
2021-06-18 00:22:29 +0200

    updated lore

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const intructions = document.getElementById("instance");
 	instructions.innerHTML = "<h1>Welcome to:Snake Invader</h1>" +
     "<h4> (Click anywhere to start)</h4>" +
     "<h2>Instructions</h2>"+ 
-    "<p>You are a rookie pilot aboard the Transdimensional Interstellar Neutron Observer (T.I.N.O) Spacecraft. Your head pilots have been knocked out from stress of studying for\
-    their pilot exams which are taking place on Monday the 21st!(The exam board has no mercy on them :( ) Your job is to fly the ship and collect Past Paper tokens so they can buy past papers to help them \
-    study when they wake up! Youre new to the pilot game but youre a quick learner. Goodluck Captain Heres your manual</p>"+
+    "<p>You are a rookie pilot Ricch Heard with 10 PhDs in everyfield aboard the Transdimensional Interstellar Neutron Observer (T.I.N.O) Spacecraft. Your head pilots Darth Stevovo and PRAvenman VESHim'l have been knocked out from stress of studying for\
+    their pilot exams which are taking place on Monday the 21st!(The Cross-dimensional Orbital Milky-way Spacemen (C.O.M.S) board has no mercy on them :( ) Your job is to fly the ship and collect Past Paper tokens so they can buy past papers to help them \
+    study when they wake up! Youre new to the pilot game but youre a quick learner. Goodluck Captain! Heres your manual</p>"+
     "<div id='leftHandControls'> <h3> Left Hand controls </h3> " +
         "<div>Tilt Left: A</div>" +
         "<div>Tilt Right:D</div>"+
